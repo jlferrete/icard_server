badffba838dcda77e26dd1dec1bbd3d88e89551b
Jlferrete <joseluisferrete@gmail.com>
2022-04-01 18:24:43 +0200

    Configurando la documentación de la API

--- icard/icard/urls.py
+++ icard/icard/urls.py
@@ -15,7 +15,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="iCard - ApiDoc",
+      default_version='v1',
+      description="documentación de la api de iCard",
+      terms_of_service="https://jlferrete.com",
+      contact=openapi.Contact(email="contacto@jlferrete.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', schema_view.with_ui('swagger', 
+        cache_timeout=0), name='schema-swagger-ui'),
+    path('redocs/', schema_view.with_ui('redoc', 
+        cache_timeout=0), name='schema-redoc'),
+
 ]
