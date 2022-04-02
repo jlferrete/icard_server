bb86cf8fb93656d8360334bca41037ed9fbefc3c
Jlferrete <joseluisferrete@gmail.com>
2022-04-02 13:41:45 +0200

    Creando el CRUD de usuarios

--- icard/icard/urls.py
+++ icard/icard/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
+from users.api.router import router_user
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -36,5 +39,6 @@ urlpatterns = [
         cache_timeout=0), name='schema-swagger-ui'),
     path('redocs/', schema_view.with_ui('redoc', 
         cache_timeout=0), name='schema-redoc'),
+    path('api/', include(router_user.urls))
 
 ]
